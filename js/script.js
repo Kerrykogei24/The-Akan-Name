a966098f16abf8085410b94a4f23529316bf25a6
Kerrykogei24 <kerrykomar@gmail.com>
2020-10-01 22:20:17 +0300

    added form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwam"];
 const femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-const weekDay = weekDays[4];
-// function myFunction() {
-document.getElementById("demo").innerHTML = weekDay;
-// };
+
+function myFunction() {
+    document.getElementById("Birthday").innerHTML = Birthday;
+
+}
